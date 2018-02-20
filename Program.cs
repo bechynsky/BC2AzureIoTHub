830fdac115ac39af944486472b0aa9a29da12360
Stepan Bechynsky <stepan@bechynsky.cz>
2018-02-20 17:35:38 +0100

    Init of .Net Core 2 app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BC2AzureIoTHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
