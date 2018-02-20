84daca30e90db3a7e66162778a2cc6003b181d39
Stepan Bechynsky <stepan@bechynsky.cz>
2018-02-20 18:08:56 +0100

    Device client

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace BC2AzureIoTHub
 {
     class Program
     {
+        private static MqttClient _mqttClient = new MqttClient("localhost");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _mqttClient.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+
+            _mqttClient.Connect("BC01");
+            
+            if (_mqttClient.IsConnected)
+            {
+                _mqttClient.Subscribe(new string[] { "#" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+                Console.WriteLine("MQTT connected");
+            }
+
+            while (true) {}
         }
+
+        private static void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        {
+            string[] topicParts = e.Topic.Split('/');
+
+            if (topicParts.Length != 5)
+            {
+                return;
+            }
+
+            string key = topicParts[4];
+            string value = System.Text.Encoding.Default.GetString(e.Message);
+
+            string data = "{'" + key + "':" + value + "}";            
+
+            Console.WriteLine(data);            
+        }  
     }
 }
