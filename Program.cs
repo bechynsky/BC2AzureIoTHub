e9a4b482f10c1ad0f39b0083f64135fdf6cea253
Stepan Bechynsky <stepan@bechynsky.cz>
2018-02-23 18:33:53 +0100

    Added configuration

--- Program.cs
+++ Program.cs
@@ -5,15 +5,33 @@ using Microsoft.Azure.Devices.Client;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
 namespace BC2AzureIoTHub
 {
     class Program
     {
         private static MqttClient _mqttClient = new MqttClient("localhost");
         private static DeviceClient _deviceClient = null;
-
+        private static IConfiguration _configuration { get; set; }
+        
         static void Main(string[] args)
         {
+            /*
+                connection.json
+
+                {
+                    "connectionString": ""
+                }
+             */
+            var builder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("connection.json");
+            _configuration = builder.Build();
+
+            _deviceClient = DeviceClient.CreateFromConnectionString(_configuration["connectionString"]);
+
             _mqttClient.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
 
             _mqttClient.Connect("BC01");
@@ -27,7 +45,7 @@ namespace BC2AzureIoTHub
             while (true) {}
         }
 
-        private static void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
+        private static async void Client_MqttMsgPublishReceived(object sender, uPLibrary.Networking.M2Mqtt.Messages.MqttMsgPublishEventArgs e)
         {
             string[] topicParts = e.Topic.Split('/');
 
@@ -39,7 +57,9 @@ namespace BC2AzureIoTHub
             string key = topicParts[4];
             string value = System.Text.Encoding.Default.GetString(e.Message);
 
-            string data = "{'" + key + "':" + value + "}";            
+            string data = "{'" + key + "':" + value + "}";
+            Message payload = new Message(System.Text.Encoding.UTF8.GetBytes(data));
+            await _deviceClient.SendEventAsync(payload);
 
             Console.WriteLine(data);            
         }  
